3e8c476d04522feb65e6d3fe21726c5b4cb7eee9
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:05:58 +0500

    Add files via upload

---
+++ Assignment9/main.js
@@ -0,0 +1,6 @@
+/*
+Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your favorite number. Print that message.
+ */
+var num = 10;
+console.log("My favourite number is : ".concat(num));
---
+++ Assignment9/main.ts
@@ -0,0 +1,8 @@
+/*
+Favorite Number: Store your favorite number in a variable.
+ Then, using that variable, create a message that reveals your favorite number. Print that message.
+ */
+
+let num = 10
+
+console.log(`My favourite number is : ${num}`);
