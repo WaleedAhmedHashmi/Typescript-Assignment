24e2fac0ff137208b019aafadf85e56e5a092583
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:03:50 +0500

    Add files via upload

---
+++ Assignment4/main.js
@@ -0,0 +1,10 @@
+/*
+
+Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+ Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+*/
+var cname = "Albert Einstein";
+var Quote = "A person who never made a mistake never tried anything new.";
+console.log("".concat(cname, " once said, \"").concat(Quote, "\""));
---
+++ Assignment4/main.ts
@@ -0,0 +1,13 @@
+/*
+
+Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+ Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+*/
+
+let cname = "Albert Einstein";
+
+let Quote = "A person who never made a mistake never tried anything new.";
+
+console.log(`${cname} once said, "${Quote}"`)
