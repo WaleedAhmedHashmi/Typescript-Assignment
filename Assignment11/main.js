065e6ae5f7995b54401b822cba9b3a44551eea05
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:06:57 +0500

    Add files via upload

---
+++ Assignment11/main.js
@@ -0,0 +1,11 @@
+/*
+
+Names: Store the names of a few of your friends in a array called names.
+Print each person’s name by accessing each element in the list, one at a time.
+
+*/
+var fry = ["nayab", "tausif", "bilal"];
+for (var _i = 0, fry_1 = fry; _i < fry_1.length; _i++) {
+    var cname = fry_1[_i];
+    console.log(cname);
+}
---
+++ Assignment11/main.ts
@@ -0,0 +1,13 @@
+/*
+
+Names: Store the names of a few of your friends in a array called names. 
+Print each person’s name by accessing each element in the list, one at a time.
+
+*/
+
+
+let fry = ["nayab","tausif","bilal"]
+
+for(let cname of fry){
+    console.log(cname);
+}
