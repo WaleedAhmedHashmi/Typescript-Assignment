5b32ce5731232deae25a88aeb85741ba89c231e7
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:02:03 +0500

    Add files via upload

---
+++ Assignment2/main.js
@@ -0,0 +1,5 @@
+//Personal Message: Store a person’s name in a variable, and 
+//print a message to that person. Your message should be simple, such as,
+//“Hello Eric, would you like to learn some Python today?”
+var cname = "waleed";
+console.log("Hello ".concat(cname, " , would you like to learn some Python today?"));
