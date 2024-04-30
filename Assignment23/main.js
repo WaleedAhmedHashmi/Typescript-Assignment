70ac8a20769b75b32a89204c83a7553a6002477e
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:11:41 +0500

    Add files via upload

---
+++ Assignment23/main.js
@@ -0,0 +1,21 @@
+/*
+Conditional Tests: Write a series of conditional tests.
+Print a statement describing each test and your prediction for the
+results of each test.
+ Your code should look something like this:
+
+*/
+// let car = "Mehran";
+// if (car == "Mehran") {
+//     console.log(`Your car is == ${car}`)   
+// }
+// else{
+//     console.log("Wrong Car")
+// }
+var Car = 'Supra';
+if (Car == 'Supra') {
+    console.log("YOur Car is == ".concat(Car));
+}
+else {
+    console.log("Wrong Guess of Car");
+}
---
+++ Assignment23/main.ts
@@ -0,0 +1,18 @@
+/*
+Conditional Tests: Write a series of conditional tests. 
+Print a statement describing each test and your prediction for the 
+results of each test.
+ Your code should look something like this:
+
+*/
+
+let car = "Mehran";
+
+if (car == "Mehran") {
+    console.log(`Your car is == ${car}`)   
+}
+
+else{
+    console.log("Wrong Car")
+}
+
