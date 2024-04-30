b17d2e79b25ad967281bf4faa9fe63319daa007a
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:12:26 +0500

    Add files via upload

---
+++ Assignment25/main.ts
@@ -0,0 +1,22 @@
+/*
+Alien Colors #1: Imagine an alien was just shot down in a game.
+ Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+*/
+
+let alien_color = "green";
+
+if (alien_color == "green") {
+   console.log("the player just earned 5 points.")
+}
+else{
+  console.log("The player could not earned 5 points.")
+}
+
+let alien_color1 = "red";
+
+if (alien_color1 == "green") {
+   console.log("The player could not earned 5 points.")
+}
+else{
+  console.log("No output")
+}
