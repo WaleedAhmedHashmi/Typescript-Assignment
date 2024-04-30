3b60c084aa986d9cb9e7d494ba666edcd59934f3
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:10:40 +0500

    Add files via upload

---
+++ Assignment20/main.js
@@ -0,0 +1,9 @@
+/*
+Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.
+*/
+var country = ["Pakistan", "Australia", "England"];
+for (var _i = 0, country_1 = country; _i < country_1.length; _i++) {
+    var names = country_1[_i];
+    console.log("".concat(names));
+}
