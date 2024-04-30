e7f89bc2f438df090b5ccf5940f61ebc56a1a126
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:04:57 +0500

    Add files via upload

---
+++ Assignment6/main.js
@@ -0,0 +1,12 @@
+/*
+
+Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+ Make sure you use each character combination, "\t" and "\n", at least once.
+  Print the name once, so the whitespace around the name is displayed.
+Then print the name after striping the white spaces.
+
+*/
+var fname = " \t Usman \n Khan \t ";
+console.log(fname);
+var stripped = "Amir";
+console.log(stripped.trim());
---
+++ Assignment6/main.ts
@@ -0,0 +1,16 @@
+/*
+
+Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name.
+ Make sure you use each character combination, "\t" and "\n", at least once.
+  Print the name once, so the whitespace around the name is displayed. 
+Then print the name after striping the white spaces.
+
+*/
+
+let fname = " \t Usman \n Khan \t ";
+
+ console.log( fname);
+
+let stripped = "Amir";
+
+console.log(stripped.trim());
