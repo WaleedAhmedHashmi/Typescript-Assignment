5222d95aec39331136cae95fd6d017e8881b1746
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:05:14 +0500

    Add files via upload

---
+++ Assignment7/main.js
@@ -0,0 +1,10 @@
+/*
+
+Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8
+. Be sure to enclose your operations in print statements to see the results.
+
+*/
+console.log("Addition of 2+6 is = ", 4 + 4);
+console.log("Subtraction of 10-2 is =", 10 - 2);
+console.log("Multiplication of 2*4 is =", 2 * 4);
+console.log("Subtraction of 16/2 is =", 16 / 2);
