580ceb392e8b8d5cd15b2ae7108a4d4cf2187fbc
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:11:24 +0500

    Add files via upload

---
+++ Assignment22/main.js
@@ -0,0 +1,12 @@
+/*
+
+Intentional Error: If you haven’t received an array index error in one of your
+ programs yet, try to make one happen. Change an index in one of your
+ programs to produce an index error.
+ Make sure you correct the error before closing the program.
+
+*/
+var car = ["Mercedez", "Cultus", "Audi"];
+console.log(car[3]);
+var car1 = ["Mercedez", "Cultus", "Audi"];
+console.log(car1[1]);
---
+++ Assignment22/main.ts
@@ -0,0 +1,18 @@
+/*
+
+Intentional Error: If you haven’t received an array index error in one of your
+ programs yet, try to make one happen. Change an index in one of your 
+ programs to produce an index error.
+ Make sure you correct the error before closing the program.
+
+*/
+
+
+let car = ["Mercedez","Cultus","Audi"]
+
+console.log(car[3]);
+
+
+let car1 = ["Mercedez","Cultus","Audi"]
+
+console.log(car1[1]);
