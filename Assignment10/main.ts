0fe7aac156789d83820f344601a7f2f3091f7790
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:06:23 +0500

    Add files via upload

---
+++ Assignment10/main.ts
@@ -0,0 +1,18 @@
+/*
+Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+ If you don’t have anything specific to write because your programs are too simple at this point, 
+ just add your name and the current date at the top of each program file. 
+Then write one sentence describing what the program does.
+*/
+
+//First Program  --  it Stores my favorite number in a Variable and displays on console with a message!@!!!!!
+
+let num = 10;
+
+console.log(`My favourite number is : ${num}`);
+
+//Second Program  -- a person’s name in a variable, and print a message to that person. 
+
+let fname = "waleed";
+
+console.log(fname);
