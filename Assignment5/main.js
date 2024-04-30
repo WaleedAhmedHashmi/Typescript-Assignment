210ba79cc9d16f1545d3b7cce68d8d0700940eea
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:04:20 +0500

    Add files via upload

---
+++ Assignment5/main.js
@@ -0,0 +1,8 @@
+/*
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+ Then compose your message and store it in a new variable called message. Print your message.
+*/
+var famous_person = "Ali";
+var Quote = "Confidence is the best way to success.";
+var message = "".concat(famous_person, " once Said , \"").concat(Quote, "\"");
+console.log(message);
---
+++ Assignment5/main.ts
@@ -0,0 +1,11 @@
+/*
+Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+ Then compose your message and store it in a new variable called message. Print your message.
+*/
+
+let famous_person = "Ali";
+
+let Quote = "Confidence is the best way to success.";
+
+let message = `${famous_person} once Said , "${Quote}"`;
+console.log(message);
