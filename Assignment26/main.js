1aba80c9553544ae1d251be276aa366a2af052d6
WaleedAhmedHashmi <107610874+WaleedAhmedHashmi@users.noreply.github.com>
2024-04-30 16:12:48 +0500

    Add files via upload

---
+++ Assignment26/main.js
@@ -0,0 +1,18 @@
+/*
+Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+*/
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("Player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("The player could not earned 5 points for shooting the alien");
+}
+var alien_color_1 = "red";
+if (alien_color_1 == "green") {
+    console.log("The player just earned 5 points");
+}
+else {
+    console.log("The player just earned 10 points");
+}
